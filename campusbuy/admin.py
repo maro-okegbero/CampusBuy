0d2f24c0b7c63114770196a1a2374470b9e36dd9
marolicious1 <marookegbero@gmail.com>
2018-01-27 10:21:51 +0100

    Campus Buy app, first commit

---
+++ campusbuy/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ItemAdvert
+
+admin.site.register(ItemAdvert)
+# Register your models here.
---
+++ campusbuy/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+
+
+class ItemAdvert(models.Model):
+    Seller_Name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    Item = models.CharField(max_length= 200)
+    Description = models.TextField()
+    Created_data = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(
+    blank=True, null=True)
+
+    def published(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.Item
+
+
+
+# Create your models here.
