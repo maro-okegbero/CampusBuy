5439c589fdbc8c1f9e592ffe41b48882b81f12e1
marolicious1 <marookegbero@gmail.com>
2018-02-05 00:06:03 +0100

    Changed the homepage and added a new field in the Models.py

--- campusbuy/models.py
+++ campusbuy/models.py
@@ -6,6 +6,7 @@ class ItemAdvert(models.Model):
     Seller_Name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     Item = models.CharField(max_length= 200)
     Description = models.TextField()
+    Asking_Price = models.FloatField()
     Created_data = models.DateTimeField(default = timezone.now)
     published_date = models.DateTimeField(
     blank=True, null=True)
