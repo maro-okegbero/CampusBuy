68d81b77b1009815183fd73630c6280f8ddd57c4
marolicious1 <marookegbero@gmail.com>
2019-01-13 01:29:37 +0100

    Removed elasticsearch service, implemented FTS with the django.contrib.postgre.search module. Deploying to Pythonanywhere

--- campusbuy/urls.py
+++ campusbuy/urls.py
@@ -4,7 +4,10 @@ from campusbuy.models import Category, Advert
 urlpatterns = [
     path('', views.Homepage, name='Homepage'),
     path('Post_Ad', views.PostAd, name='PostAd'),
-    path('<category_name>', views.ViewAd, name='ViewAd')
+    path('Search', views.Search, name='Search'),
+    path('<category_name>', views.ViewAd, name='ViewAd'),
+
+
 
 ]
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '$-1%w5fwoea+$_h_iqb!&5-5*vw^puya74k(@e09cs1zbd2kyb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'campusbuy.pythonanywhere.com', '192.168.43.82']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '192.168.43.82']
 
 
 # Application definition
@@ -37,22 +38,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'haystack',
     'campusbuy',
-    'crispy_forms',
-    'search',
-    'django_elasticsearch_dsl',
 
     ]
 
-ELASTICSEARCH_DSL = {
 
+
+HAYSTACK_CONNECTIONS = {
     'default': {
-        'hosts': 'localhost:9200'
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
 
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,9 +92,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
 'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'campusbuy',
-        'USER': 'code_majestic',
+        'USER': 'postgres',
         'PASSWORD': 'orangesquash',
         'HOST': 'localhost',
         'PORT': '',
@@ -139,4 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
