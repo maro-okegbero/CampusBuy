52ccec2bdcbefad84ec85c3206b0c7cd39e8dc31
marolicious1 <marookegbero@gmail.com>
2019-02-24 21:29:53 +0100

    Fixing template error

--- campusbuy/urls.py
+++ campusbuy/urls.py
@@ -3,9 +3,9 @@ from .import views
 from campusbuy.models import Category, Advert
 urlpatterns = [
     path('', views.Homepage, name='Homepage'),
-    path('Post_Ad', views.PostAd, name='PostAd'),
-    path('search', views.Search, name='Search'),
     path('<category_name>', views.ViewAd, name='ViewAd'),
+    path('Post_Ad', views.PostAd, name='PostAd'),
+    path('search', views.Search, name='Search')
 
 
 
