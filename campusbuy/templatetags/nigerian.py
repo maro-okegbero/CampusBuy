c463a0d557d2adc5b8cca013aff9e1835f014c99
CODEMAJESTIC <marookegbero@gmail.com>
2019-08-20 18:05:01 +0100

    Fixed a bug

--- campusbuy/templatetags/nigerian.py
+++ campusbuy/templatetags/nigerian.py
@@ -6,7 +6,6 @@ register = template.Library()
 @register.filter(name = 'nigerianalize')
 
 def nigerianalize(value):
-
     newvalue = value.strip('0')
     niaja = "+234" + newvalue
     return niaja
