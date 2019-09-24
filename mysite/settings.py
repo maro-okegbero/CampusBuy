df46ff8e119116a2d0906ae0004bc182abb733d9
OMS <marookegbero@gmail.com>
2019-09-24 13:56:11 +0100

    commiting stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 #import cloudinary
 #import cloudinary.uploader
 import cloudinary.api
-from environs import Env
+#from environs import Env
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
