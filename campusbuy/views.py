1d5d3ee87988e6a0027c447d40ba70373e3d8b83
OMS <marookegbero@gmail.com>
2019-09-30 15:59:57 +0100

    UI FIXES

--- campusbuy/views.py
+++ campusbuy/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
@@ -8,17 +8,25 @@ from .forms import PostAdForm
 from django.contrib.postgres.search import SearchVector
 
 
-
 # The view the view baby!
 
-def Homepage(request):
+def homepage(request):
+    """
+    This view is responsible for displaying the contents of the homepage
+    :param request:
+    :return:
+    """
     Categories = Category.objects.all()
-    return render(request, 'campusbuy/index.html', {'Categories': Categories} )
+    return render(request, 'campusbuy/index.html', {'Categories': Categories})
 
 
+def viewAd(request, category_name):
+    """
 
-
-def ViewAd(request, category_name):
+    :param request:
+    :param category_name:
+    :return: All the ads in a named category
+    """
     try:
         kategory = Category.objects.get(Name=category_name)
         MyList = kategory.advert_set.all()
@@ -36,35 +44,36 @@ def ViewAd(request, category_name):
     except Category.DoesNotExist:
         raise Http404("Category Does Not Exist")
 
-def PostAd(request):
 
-        if request.method == "POST":
-            form = PostAdForm(request.POST, request.FILES)
-            if form.is_valid():
-                post = form.save()
-                post.published_date = timezone.now()
-                post.save()
-                return redirect('Homepage')
-            else:
-                return  render('<h1> Hold on a minute, something went wrong. Try posting again</h1>')
-
+def postAd(request):
+    if request.method == "POST":
+        form = PostAdForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            post.published_date = timezone.now()
+            post.save()
+            return viewAd(request, category_name=post.category)
         else:
+            return render('<h1> Hold on a minute, something went wrong. Try posting again</h1>')
 
-            form = PostAdForm()
-        return render(request, 'campusbuy/new_advert.html', {'form': form})
+    else:
+
+        form = PostAdForm()
+    return render(request, 'campusbuy/new_advert.html', {'form': form})
 
 
 def search(request):
     query = request.GET.get('q')
     if query:
-        results = Advert.objects.annotate(search=SearchVector('Item', 'Description', 'Seller_Name'),).filter(search = query)
+        results = Advert.objects.annotate(search=SearchVector('Item', 'Description', 'Seller_Name'), ).filter(
+            search=query)
     else:
         results = Advert.objects.filter(Seller_Name__startswith="/")
 
     return render(request, 'campusbuy/search.html', {'results': results})
 
 
-def handler404(request, exception,template_name="404_Error.html"):
+def handler404(request, exception, template_name="404_Error.html"):
     response = render_to_response("campusbuy/404_Error.html")
     response.status_code = 404
     return response
